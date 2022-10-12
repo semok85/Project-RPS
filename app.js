23a48fc5cfcd285a3a917430d7d264958ec62d07
sem oktavianus <sem.oktavianus@gmail.com>
2022-10-12 14:55:11 +0700

    revision adding score for player and computer

--- app.js
+++ app.js
@@ -2,43 +2,49 @@ const getComputerChoice = () => {
   let randomNum = Math.floor(Math.random() * 3) + 1;
 
   if (randomNum === 1) {
-    return "Rock";
+    return "rock";
   } else if (randomNum === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
+};
+let myScore = 0;
+let comScore = 0;
+const getPlayerInput = () => {
+  return prompt("Please Select Paper, Scissor or Paper").toLocaleLowerCase();
 };
 
 const playRsp = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
     return `Draw, ${playerSelection} equal with ${computerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    myScore++;
     return `You Win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
+    comScore++;
     return `You Lose, ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    comScore++;
     return `You Lose, ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    myScore++;
     return `You Win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    comScore++;
     return `You Lose, ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    myScore++;
     return `You Win, ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return "Please select the right one";
+  }
+};
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRsp(getPlayerInput(), getComputerChoice()));
+    console.log(`Score : You ${myScore} vs Computer ${comScore}`);
   }
 };
 
-playerSelection = prompt(
-  "Please Select Paper, Scissor or Paper"
-).toLocaleLowerCase();
-computerSelection = getComputerChoice().toLocaleLowerCase();
-
-if (
-  playerSelection === "paper" ||
-  playerSelection === "rock" ||
-  playerSelection === "scissors"
-) {
-  console.log(playRsp(playerSelection, computerSelection));
-} else {
-  location.reload();
-}
+game();
